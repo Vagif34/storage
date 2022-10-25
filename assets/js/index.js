a9e4498e51ae4c22826a9a01a74ce5184efd81dd
Vagif34 <vagifrg@code.edu.az>
2022-10-25 17:32:15 +0400

    new task

---
+++ assets/js/index.js
@@ -0,0 +1,23 @@
+const value=document.querySelector(".register")
+const inValue=document.querySelector("#full-name")
+const enterToValue=document.querySelector("#username")
+const intoValue=document.querySelector("#email")
+const inputValue=document.querySelector("#phone-number")
+const entValue=document.querySelector("#password")
+const enterValue=document.querySelector("#confrim-password")
+
+
+
+function BooleanType(namestring){
+    if(nameString){
+        return true;
+    }
+    return false;
+}
+
+
+let a=""
+console.log(a);
+locBtn.addEventList("click",() =>{
+    console.log(enterValue);
+});
